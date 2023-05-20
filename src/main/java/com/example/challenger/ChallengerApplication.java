b8f4b74e6ffa259965233b474c43fa56b7c16b80
MoonJihye <jihyer0395@gmail.com>
2023-05-21 05:30:38 +0900

    setting

---
+++ src/main/java/com/example/challenger/ChallengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.challenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengerApplication.class, args);
+	}
+
+}
