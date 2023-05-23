afd8d8655be8c7d1f2eb6d44ec63d6de68084191
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-05-23 18:21:46 +0900

    Member Builder 테스트 코드 작성

--- src/test/java/com/example/challenger/service/TeamMemberServiceTest.java
+++ src/test/java/com/example/challenger/service/TeamMemberServiceTest.java
@@ -12,6 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class TeamMemberServiceTest {
     @Autowired
@@ -22,6 +24,25 @@ class TeamMemberServiceTest {
 
     @Autowired
     TeamMemberDAO teamMemberDAO;
+
+    @Test
+    @DisplayName("Builder 테스트")
+    void testBuilder() {
+        String name = "테스트";
+        String position = "백엔드";
+        Long teamId = 14L;
+        Team team = teamDAO.selectTeam(teamId).get();
+
+        TeamMemberDto teamMemberDto = TeamMemberDto.builder()
+                .name(name)
+                .position(position)
+                .team(team)
+                .build();
+
+        assertThat(teamMemberDto.getName()).isEqualTo(name);
+        assertThat(teamMemberDto.getPosition()).isEqualTo(position);
+        assertThat(teamMemberDto.getTeam()).isEqualTo(team);
+    }
 
     @Test
     @DisplayName("새로운 멤버 추가")
